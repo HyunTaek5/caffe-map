2c6c557c44c435326b0cacac3de344da7da5932c
HyunTaek Oh <oht222@naver.com>
2024-04-11 03:53:53 +0900

    add: 커스텀 validation Pipe 적용

--- caffe-map-api/apps/front-api/src/main.ts
+++ caffe-map-api/apps/front-api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomValidationPipe } from '../../domain/common/pipe/validation.pipe';
+import { json, urlencoded } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  app.use(json({ limit: '100mb' }));
+  app.use(urlencoded({ limit: '100mb', extended: true }));
+
+  app.useGlobalPipes(new CustomValidationPipe());
+
+  await app.listen(process.env.API_PORT || 8000);
 }
 bootstrap();
