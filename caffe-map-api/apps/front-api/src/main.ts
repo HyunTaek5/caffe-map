866f2b9b4ef1c6e9646f1e98aaabfbd31b8cbc50
HyunTaek Oh <oht222@naver.com>
2024-04-12 15:33:13 +0900

    chore: import directory 수정

--- caffe-map-api/apps/front-api/src/main.ts
+++ caffe-map-api/apps/front-api/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CustomValidationPipe } from '../../domain/common/pipe/validation.pipe';
+import { CustomValidationPipe } from 'apps/domain/common/pipe/validation.pipe';
 import { json, urlencoded } from 'body-parser';
 
 async function bootstrap() {
