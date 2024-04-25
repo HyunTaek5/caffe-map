d7e5ac78684271fe77f5292ddbd5eeae51589446
HyunTaek Oh <oht222@naver.com>
2024-04-25 23:05:14 +0900

    add: 지도 생성 api 구현

--- caffe-map-api/apps/application/map/map.service.ts
+++ caffe-map-api/apps/application/map/map.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateMapDto } from 'apps/application/map/dto/req/create-map.dto';
+import { crawlKakaoMap } from 'apps/domain/map/crawlKakaoMap';
 
 @Injectable()
 export class MapService {
   constructor() {}
+  /**
+   * 지도 생성
+   * - 입력받은 지도 주소에서 크롤링
+   * - 크롤링한 데이터를 중복체크 및 저장
+   *
+   * @param dto CreateMapDto
+   */
+  async createMap(dto: CreateMapDto) {
+    await crawlKakaoMap(dto.mapShareUrl);
+  }
 }
--- caffe-map-api/apps/front-api/src/map/map.controller.ts
+++ caffe-map-api/apps/front-api/src/map/map.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { MapService } from 'apps/application/map/map.service';
+import { CreateMapDto } from 'apps/application/map/dto/req/create-map.dto';
 
 @Controller('maps')
 export class MapController {
   constructor(private readonly mapService: MapService) {}
+
+  @Post()
+  async createMap(@Body() dto: CreateMapDto) {
+    await this.mapService.createMap(dto);
+  }
 }
