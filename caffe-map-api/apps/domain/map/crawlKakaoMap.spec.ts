7f2c142fa6cb1696fda5882435e3be5afe64046e
HyunTaek Oh <oht222@naver.com>
2024-04-29 00:40:42 +0900

    init: crawlKakaoMap 테스트 파일 테스트분기 작성

---
+++ caffe-map-api/apps/domain/map/crawlKakaoMap.spec.ts
@@ -0,0 +1,5 @@
+describe('crawlKakaoMap', () => {
+  it('즐겨찾기 공유 주소가 제공된 경우, api 크롤링이 되어야함', async () => {});
+  it('즐겨찾기 주소가 제공된 경우, api 크롤링이 되어야함', async () => {});
+  it('잘못된 주소가 제공된 경우, ConflictException 이 발생해야함', async () => {});
+});
