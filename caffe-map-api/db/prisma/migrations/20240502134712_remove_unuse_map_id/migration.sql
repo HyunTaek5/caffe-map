0d55a9a803d9fbbfcea139e91d0f0956d09b1a8c
HyunTaek Oh <oht222@naver.com>
2024-05-02 22:47:47 +0900

    fix: place 테이블 mapId 칼럼 삭제

---
+++ caffe-map-api/db/prisma/migrations/20240502134712_remove_unuse_map_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `map_id` on the `place` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `place` DROP COLUMN `map_id`;
