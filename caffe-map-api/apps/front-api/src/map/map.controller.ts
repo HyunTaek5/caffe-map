ba5f3fbfc916bab55d0d58de4f53d29a58523ac3
HyunTaek Oh <oht222@naver.com>
2024-05-05 15:52:47 +0900

    add: 지도 생성 api 구현

--- caffe-map-api/apps/front-api/src/map/map.controller.ts
+++ caffe-map-api/apps/front-api/src/map/map.controller.ts
@@ -1,13 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { MapService } from 'apps/application/map/map.service';
 import { CreateMapDto } from 'apps/application/map/dto/req/create-map.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('maps')
 export class MapController {
   constructor(private readonly mapService: MapService) {}
 
+  @ApiOperation({
+    summary: '지도 생성',
+    description: '지도 생성',
+  })
   @Post()
-  async createMap(@Body() dto: CreateMapDto) {
+  async createMap(@Body() dto: CreateMapDto): Promise<void> {
     await this.mapService.createMap(dto);
   }
 }
