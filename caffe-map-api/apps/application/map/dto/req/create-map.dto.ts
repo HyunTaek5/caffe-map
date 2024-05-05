21b8924c192d3fa9730eb84162acbac3d4bcb8a8
HyunTaek Oh <oht222@naver.com>
2024-05-06 01:34:25 +0900

    add: 지도 생성 요청 DTO에서 정규식으로 유효한 카카오맵 주소인지 검증

--- caffe-map-api/apps/application/map/dto/req/create-map.dto.ts
+++ caffe-map-api/apps/application/map/dto/req/create-map.dto.ts
@@ -1,4 +1,4 @@
-import { IsString } from 'class-validator';
+import { IsString, Matches } from 'class-validator';
 
 export class CreateMapDto {
   /**
@@ -7,5 +7,8 @@ export class CreateMapDto {
    * @example https://kko.to/2kwn5Ap-5r
    */
   @IsString()
+  @Matches(/^https?:\/\/(www\.)?(kko\.to\/\w+|map\.kakao\.com\/)/, {
+    message: '올바른 카카오맵 즐겨찾기 공유 주소를 입력해주세요.',
+  })
   mapShareUrl: string;
 }
