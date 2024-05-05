1d7d79729c72e2a47464d0d1ed6424625d9f309f
HyunTaek Oh <oht222@naver.com>
2024-05-05 15:52:42 +0900

    add: 지도 생성 로직 구현

--- caffe-map-api/apps/application/map/map.module.ts
+++ caffe-map-api/apps/application/map/map.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MapService } from './map.service';
+import { PrismaService } from 'apps/application/prisma/prisma.service';
 
 @Module({
-  providers: [MapService],
+  providers: [MapService, PrismaService],
   exports: [MapService],
 })
 export class MapModule {}
--- caffe-map-api/apps/application/map/map.service.ts
+++ caffe-map-api/apps/application/map/map.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMapDto } from 'apps/application/map/dto/req/create-map.dto';
 import { crawlKakaoMap } from 'apps/domain/map/crawlKakaoMap';
+import { PrismaService } from 'apps/application/prisma/prisma.service';
+import {
+  CrawlPlaceResult,
+  ResponseCrawlData,
+} from 'apps/domain/map/type/crawlPlace.type';
 
 @Injectable()
 export class MapService {
-  constructor() {}
+  constructor(private readonly prisma: PrismaService) {}
   /**
    * 지도 생성
    * - 입력받은 지도 주소에서 크롤링
@@ -12,7 +17,69 @@ export class MapService {
    *
    * @param dto CreateMapDto
    */
-  async createMap(dto: CreateMapDto) {
-    await crawlKakaoMap(dto.mapShareUrl);
+  async createMap(dto: CreateMapDto): Promise<void> {
+    const crawlData: ResponseCrawlData = await crawlKakaoMap(dto.mapShareUrl);
+
+    const mapInfo = crawlData.mapInfo[0];
+
+    let mapId: number;
+    let placeId: number;
+
+    // 지도 중복 체크 후 없으면 생성
+    const existMap = await this.prisma.map.findFirst({
+      where: {
+        name: mapInfo.title,
+      },
+    });
+
+    if (!existMap) {
+      const { id: createMapId } = await this.prisma.map.create({
+        data: {
+          name: mapInfo.title,
+          kakaoId: mapInfo.folderId.toString(),
+        },
+      });
+
+      mapId = createMapId;
+    } else {
+      // 이미 존재하는 지도라면 해당 지도의 id를 가져옴
+      mapId = existMap.id;
+    }
+
+    const promises = crawlData.placeList.map(
+      async (crawlPlace: CrawlPlaceResult) => {
+        const existPlace = await this.prisma.place.findFirst({
+          where: {
+            kakaoId: crawlPlace.key,
+          },
+        });
+
+        if (!existPlace) {
+          const { id: createPlaceId } = await this.prisma.place.create({
+            data: {
+              kakaoId: crawlPlace.key,
+              name: crawlPlace.display1,
+              address: crawlPlace.display2,
+              latitude: crawlPlace.x,
+              longitude: crawlPlace.y,
+            },
+          });
+
+          placeId = createPlaceId;
+        } else {
+          placeId = existPlace.id;
+        }
+
+        // 지도와 장소 연결
+        await this.prisma.placeMap.create({
+          data: {
+            mapId: mapId,
+            placeId: placeId,
+          },
+        });
+      },
+    );
+
+    await Promise.all(promises);
   }
 }
