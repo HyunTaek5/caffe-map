327d2927321c7bb05e72f41fc6b1e9492f7b2e8d
HyunTaek Oh <oht222@naver.com>
2024-05-06 17:20:49 +0900

    add: api 스웨거 maps 태그 추가

--- caffe-map-api/apps/front-api/src/map/map.controller.ts
+++ caffe-map-api/apps/front-api/src/map/map.controller.ts
@@ -1,8 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { MapService } from 'apps/application/map/map.service';
 import { CreateMapDto } from 'apps/application/map/dto/req/create-map.dto';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('maps')
 @Controller('maps')
 export class MapController {
   constructor(private readonly mapService: MapService) {}
