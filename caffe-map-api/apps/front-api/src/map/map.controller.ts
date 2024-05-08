e7ca26108718bc4650182759cc529634847105a0
HyunTaek Oh <oht222@naver.com>
2024-05-09 02:51:05 +0900

    add: 지도 목록 조회 api 구현

--- caffe-map-api/apps/front-api/src/map/map.controller.ts
+++ caffe-map-api/apps/front-api/src/map/map.controller.ts
@@ -1,12 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { MapService } from 'apps/application/map/map.service';
 import { CreateMapDto } from 'apps/application/map/dto/req/create-map.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { GetMapListDto } from 'apps/application/map/dto/req/get-map-list.dto';
 
 @ApiTags('maps')
 @Controller('maps')
 export class MapController {
   constructor(private readonly mapService: MapService) {}
+
+  @ApiOperation({
+    summary: '지도 목록 조회',
+    description: '지도 목록 조회',
+  })
+  @Get()
+  async getMapList(@Query() dto: GetMapListDto) {
+    return this.mapService.getMapList(dto);
+  }
 
   @ApiOperation({
     summary: '지도 생성',
