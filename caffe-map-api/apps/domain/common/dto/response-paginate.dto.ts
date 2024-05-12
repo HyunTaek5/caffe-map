d8dc0337723a935e46f849f782d6bec6e450081f
HyunTaek Oh <oht222@naver.com>
2024-05-12 14:07:13 +0900

    add: 기본 paging response swagger타입 적용

--- caffe-map-api/apps/domain/common/dto/response-paginate.dto.ts
+++ caffe-map-api/apps/domain/common/dto/response-paginate.dto.ts
@@ -1,8 +1,51 @@
 import { PaginationMetaData } from 'apps/domain/common/dto/pagination-meta-data.dto';
 
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from '@nestjs/common';
+
 export interface IPaginated<T> {
   items: T[];
   pagination: PaginationMetaData;
+}
+
+export function BasePaginatedDto<E>(
+  DtoClass: Type<E>,
+  resourceName?: string,
+): Type<IPaginated<E>> {
+  class PaginatedHost<D> implements IPaginated<D> {
+    @ApiProperty({ isArray: true, type: () => DtoClass })
+    items: D[];
+
+    @ApiProperty({ type: () => PaginationMetaData })
+    pagination: PaginationMetaData;
+
+    constructor(
+      items: D[],
+      totalItemCount: number,
+      currentPage: number,
+      itemsPerPage: number,
+    ) {
+      const totalPage = Math.ceil(totalItemCount / itemsPerPage);
+
+      this.items = items;
+      this.pagination = {
+        totalItemCount,
+        currentItemCount: items.length,
+        totalPage: totalPage === 0 ? 1 : totalPage,
+        currentPage,
+        itemsPerPage,
+      };
+    }
+  }
+
+  if (resourceName) {
+    Object.defineProperty(PaginatedHost, 'name', {
+      writable: false,
+      value: `Paginated${resourceName}ListDto`,
+    });
+  }
+
+  return PaginatedHost;
 }
 
 export class PaginatedDto<T> implements IPaginated<T> {
