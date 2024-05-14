1f55ac618c82a6d1b6e33cd7f935b743e5ca2484
HyunTaek Oh <oht222@naver.com>
2024-05-15 01:13:00 +0900

    add: 지도 ID로 장소 목록 paging 조회 api 구현

--- caffe-map-api/apps/front-api/src/place/place.controller.ts
+++ caffe-map-api/apps/front-api/src/place/place.controller.ts
@@ -1,9 +1,29 @@
-import { ApiTags } from '@nestjs/swagger';
-import { Controller } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query } from '@nestjs/common';
 import { PlaceService } from 'apps/application/place/place.service';
+import { GetPlacesDto } from 'apps/application/place/dto/req/get-places.dto';
+import {
+  BasePaginatedDto,
+  PaginatedDto,
+} from 'apps/domain/common/dto/response-paginate.dto';
+import { PlaceDto } from 'apps/application/place/dto/res/place.dto';
 
 @ApiTags('places')
 @Controller('places')
 export class PlaceController {
   constructor(private readonly placeService: PlaceService) {}
+
+  @ApiOperation({
+    summary: '장소 목록 조회',
+    description: '장소 목록 조회',
+    operationId: 'getPlaces',
+  })
+  @ApiOkResponse({
+    type: BasePaginatedDto(PlaceDto, 'Place'),
+    isArray: true,
+  })
+  @Get()
+  async getPlaces(@Query() dto: GetPlacesDto): Promise<PaginatedDto<PlaceDto>> {
+    return this.placeService.getPlacesByMapId(dto);
+  }
 }
