da921959e5370de95aa1bfcdfd48c9aadb6e5614
HyunTaek Oh <oht222@naver.com>
2024-05-16 02:36:48 +0900

    add: 장소 상세 조회 api 구현

--- caffe-map-api/apps/front-api/src/place/place.controller.ts
+++ caffe-map-api/apps/front-api/src/place/place.controller.ts
@@ -1,5 +1,10 @@
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { PlaceService } from 'apps/application/place/place.service';
 import { GetPlacesDto } from 'apps/application/place/dto/req/get-places.dto';
 import {
@@ -7,6 +12,7 @@ import {
   PaginatedDto,
 } from 'apps/domain/common/dto/response-paginate.dto';
 import { PlaceDto } from 'apps/application/place/dto/res/place.dto';
+import { NotFoundException } from 'apps/domain/common/exceptions/not-found.exception';
 
 @ApiTags('places')
 @Controller('places')
@@ -25,5 +31,19 @@ export class PlaceController {
   @Get()
   async getPlaces(@Query() dto: GetPlacesDto): Promise<PaginatedDto<PlaceDto>> {
     return this.placeService.getPlacesByMapId(dto);
+  }
+
+  @ApiOperation({
+    summary: '장소 상세 조회',
+    description: '장소 상세 조회',
+    operationId: 'getPlaceById',
+  })
+  @ApiNotFoundResponse({
+    description: '해당 id의 장소 정보가 없을시 해당 에러 반환',
+    type: NotFoundException('해당 장소 정보가 없습니다.'),
+  })
+  @Get('/:id')
+  async getPlaceById(@Param('id', ParseIntPipe) id: number): Promise<PlaceDto> {
+    return this.placeService.getPlaceById(id);
   }
 }
