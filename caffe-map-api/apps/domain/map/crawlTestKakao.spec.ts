fb84f93b2af14b53e506d78e7e8d5dff5529daa7
HyunTaek Oh <oht222@naver.com>
2024-06-07 20:40:33 +0900

    fix: crawl 테스트용 코드 조건 수정

--- caffe-map-api/apps/domain/map/crawlTestKakao.spec.ts
+++ caffe-map-api/apps/domain/map/crawlTestKakao.spec.ts
@@ -1,24 +1,30 @@
 import { crawlTestKakao } from '../map/crawlTestKakao';
 
 describe('crawlTestKakao', () => {
-  it('정상적으로 진행된다면 fetch가 완료되고 console에 로그가 찍혀야됨', async () => {
+  it('정상적으로 진행된다면 fetch가 완료되고 해당 결과가 리턴되어야함', async () => {
     // Given
     const mapShareAddress = 'https://google.com';
 
     // When
     const result = await crawlTestKakao(mapShareAddress);
 
-    // // Then
-    // expect(result).toThrowError('지도 크롤링 중 오류가 발생했습니다.');
+    // Then
+    expect(result.status).toEqual(200);
   });
   it('SSL 에러가 발생', async () => {
     // Given
     const mapShareAddress = 'https://kko.to/2kwn5Ap-5r';
 
-    // When
-    const result = await crawlTestKakao(mapShareAddress);
+    try {
+      // When
+      const result = await crawlTestKakao(mapShareAddress);
 
-    // // Then
-    // expect(result).toThrowError('지도 크롤링 중 오류가 발생했습니다.');
+      // Then
+      expect(result).toBeInstanceOf(Response);
+    } catch (err) {
+      // Then
+      console.log(err);
+      expect(err).toContain('[TypeError: fetch failed]');
+    }
   });
 });
